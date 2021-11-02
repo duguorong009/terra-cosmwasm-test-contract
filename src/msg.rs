0cd528e205cf506471c6b00fe130a650cef72f99
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-11-02 03:47:57 -0700

    Implement new messages

--- src/msg.rs
+++ src/msg.rs
@@ -2,26 +2,28 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-    pub count: i32,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    GetTaxRate {},
+    GetTaxCap { denom: String },
+    GetCalcWithdrawAmount { uusd_amount: u128 },
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct TaxRateResponse {
+    pub tax_rate: u128,
+    pub denominator: u128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TaxCapResponse {
+    pub tax_cap: u128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CalcPossibleWithdrawAmount {
+    pub possible_withdraw_amount: u128,
+    pub tax_amount: u128,
 }
